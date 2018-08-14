0802e4f344585a0d222206421051eac516a0a5a6
Shane Phillips <shane.phillips@gazette.com>
2018-08-14 12:41:56 -0600

    scaffold plugin

---
+++ wp-2-alexa.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     WP2Alexa
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     This plugin creates customizable, Amazon Alexa-ready feeds for WordPress posts.
+ * Author:          WP2Selceeus
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp2alexa
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         WP2Alexa
+ */
+
+// Your code starts here.
